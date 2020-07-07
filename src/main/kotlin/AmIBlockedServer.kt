5ac45820503f70aaddfb9a1e4d3aeba0f1f68356
Marvin <marv.1997@live.de>
2020-07-07 18:12:01 +0200

    Refactor code, split into logical methods and move the server into a separate class

--- src/main/kotlin/AmIBlockedServer.kt
+++ src/main/kotlin/AmIBlockedServer.kt
@@ -2,6 +2,7 @@ package de.gianttree.amiblocked
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.github.benmanes.caffeine.cache.AsyncLoadingCache
 import com.github.benmanes.caffeine.cache.Caffeine
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
@@ -45,83 +46,104 @@ val mapper: ObjectMapper = jacksonObjectMapper()
 
 val configPath: Path = Paths.get("config.json")
 
-@ExperimentalTime
 @KtorExperimentalAPI
+@ExperimentalTime
 fun main() {
+    AmIBlockedServer().apply { startServer() }
+}
 
+@ExperimentalTime
+class AmIBlockedServer {
 
-    val configuration = if (Files.exists(configPath)) {
-        mapper.readValue(configPath.toFile().readBytes(), Configuration::class.java)
-    } else {
-        Configuration.default()
-    }
-    mapper.writerWithDefaultPrettyPrinter().writeValue(configPath.toFile(), configuration)
+    private val configuration = loadConfiguration()
 
-    val databaseConfig = HikariConfig().apply {
-        jdbcUrl = configuration.database.jdbcUrl
-        driverClassName = configuration.database.driver
-        username = configuration.database.username
-        password = configuration.database.password
-        maximumPoolSize = 2
-        minimumIdle = 1
-    }
-    val database = Database.connect(HikariDataSource(databaseConfig))
-    transaction(database) {
-        SchemaUtils.createMissingTablesAndColumns(BlockedUsers)
-    }
+    private val database = loadDatabase()
 
-    val queryCache = Caffeine.newBuilder()
-        .maximumSize(1000)
-        .expireAfterWrite(12.hours.toJavaDuration())
-        .executor(Dispatchers.IO.asExecutor())
-        .buildAsync<String, String> { key ->
-            mapper.writeValueAsString(
-                transaction(database) {
-                    BlockedUser.find {
-                        (BlockedUsers.username eq key) or                       // Full match
-                                //language=RegExp
-                                (BlockedUsers.username regexp "^${Regex.escape(key)}#\\d{4}$") or // Match without discriminator
-                                (BlockedUsers.snowflake eq key)                 // Match snowflake
-                    }.mapLazy(::BlockedUserDTO).singleOrNull()            // Make sure, only one result is found
-                        ?: BlockedUserDTO.noResult(key)
-                }
-            )
+    private val queryCache = createCache()
+
+    private fun loadConfiguration(): Configuration {
+        return if (Files.exists(configPath)) {
+            mapper.readValue(configPath.toFile().readBytes(), Configuration::class.java)
+        } else {
+            Configuration.default()
+        }.also {
+            mapper.writerWithDefaultPrettyPrinter().writeValue(configPath.toFile(), it)
         }
+    }
 
+    private fun loadDatabase(): Database {
+        val databaseConfig = HikariConfig().apply {
+            jdbcUrl = configuration.database.jdbcUrl
+            driverClassName = configuration.database.driver
+            username = configuration.database.username
+            password = configuration.database.password
+            maximumPoolSize = 2
+            minimumIdle = 1
+        }
+        return Database.connect(HikariDataSource(databaseConfig)).also {
+            transaction(it) {
+                SchemaUtils.createMissingTablesAndColumns(BlockedUsers)
+            }
 
-    val server = embeddedServer(CIO, configuration.port, configuration.host) {
-        install(CallLogging)
-        install(CORS) {
-            configuration.cors.forEach {
-                host(it.host, it.schemes)
+        }
+    }
+
+    private fun createCache(): AsyncLoadingCache<String, String> {
+        return Caffeine.newBuilder()
+            .maximumSize(1000)
+            .expireAfterWrite(12.hours.toJavaDuration())
+            .executor(Dispatchers.IO.asExecutor())
+            .buildAsync<String, String> { key ->
+                mapper.writeValueAsString(
+                    transaction(database) {
+                        BlockedUser.find {
+                            (BlockedUsers.username eq key) or                       // Full match
+                                    //language=RegExp
+                                    (BlockedUsers.username regexp "^${Regex.escape(key)}#\\d{4}$") or // Match without discriminator
+                                    (BlockedUsers.snowflake eq key)                 // Match snowflake
+                        }.mapLazy(::BlockedUserDTO).singleOrNull()            // Make sure, only one result is found
+                            ?: BlockedUserDTO.noResult(key)
+                    }
+                )
+            }
+    }
+
+    @KtorExperimentalAPI
+    fun startServer() {
+        val server = embeddedServer(CIO, configuration.port, configuration.host) {
+            install(CallLogging)
+            install(CORS) {
+                configuration.cors.forEach {
+                    host(it.host, it.schemes)
+                }
+            }
+            install(CachingHeaders) {
+                options { outgoingContent ->
+                    when (outgoingContent.contentType?.withoutParameters()) {
+                        ContentType.Application.Json -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 1.days.inSeconds.toInt()))
+                        else -> null
+                    }
+                }
+            }
+            routing {
+                post("/api/v1/find") {
+                    val params = try {
+                        this.call.receiveParameters()
+                    } catch (e: ContentTransformationException) {
+                        parametersOf()
+                    }
+                    if ("search" !in params) {
+                        this.call.respond(HttpStatusCode.BadRequest, "")
+                        return@post
+                    }
+                    val searchParam = params["search"]!!.trim()
+
+                    val response = queryCache[searchParam]
+
+                    this.call.respondText(response.await(), ContentType.Application.Json)
+                }
             }
         }
-        install(CachingHeaders) {
-            options { outgoingContent ->
-                when (outgoingContent.contentType?.withoutParameters()) {
-                    ContentType.Application.Json -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 1.days.inSeconds.toInt()))
-                    else -> null
-                }
-            }
-        }
-        routing {
-            post("/api/v1/find") {
-                val params = try {
-                    this.call.receiveParameters()
-                } catch (e: ContentTransformationException) {
-                    parametersOf()
-                }
-                if ("search" !in params) {
-                    this.call.respond(HttpStatusCode.BadRequest, "")
-                    return@post
-                }
-                val searchParam = params["search"]!!.trim()
-
-                val response = queryCache[searchParam]
-
-                this.call.respondText(response.await(), ContentType.Application.Json)
-            }
-        }
+        server.start(true)
     }
-    server.start(true)
 }
